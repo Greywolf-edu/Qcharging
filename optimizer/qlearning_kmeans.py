235ee972a018b44b2cb9321866cd5470356dd460
Greywolf-edu <ngthlong2109.edu@gmail.com>
2022-03-27 01:26:42 +0700

    Update qlearning_kmeans.py

--- optimizer/qlearning_kmeans.py
+++ optimizer/qlearning_kmeans.py
@@ -63,4 +63,4 @@ class Q_learningv2:
             # print(self.action_list[self.state])
     
     def net_partition(self, net=None, netpart_func=network_clustering):
-        self.action_list = network_clustering(self, network=net, nb_cluster=self.nb_action)
+        self.action_list = network_clustering_v2(self, network=net, nb_cluster=self.nb_action)
